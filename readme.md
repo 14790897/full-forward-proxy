097f5a07f781b4c726e9e659e0be3c1a58827c46
liuweiqing <liuweiqing147@gmail.com>
2024-08-17 17:15:19 +0800

    init

--- readme.md
+++ readme.md
@@ -6,34 +6,32 @@
 
 例如 https://forward.paperai.life/proxy/https://github.com/14790897
 
-# 详细步骤
+## 详细步骤
 
-1. 登录https://www.cloudflare.com
-2. 创建应用程序
-   ![创建应用程序](img/1createapp.png)
-3. 创建 worker（pages 麻烦一点，需要写一个 package.json 文件，但 pages 的好处是分配的域名直接可以用）
-   ![创建worker](img/2createworker.png)
-4. 点"部署"按钮
-   ![创建worker](img/3deploy.png)
-5. 编辑代码
-   ![编辑代码](img/4update.png)
-6. 把 worker.js 文件中的内容复制进去，点"保存并部署"
-   ![保存并部署](img/5save.png)
-7. (可选) 添加自定义域
+1. 创建 KV
+   ```sh
+   wrangler kv:namespace create full_forward
+   wrangler kv:key put --binding=full_forward  "/" ./full-forward-html/index.html
+   wrangler kv:key put --binding=full_forward  "/service-worker.js" ./full-forward-html/service-worker.js
+   ```
+2. 部署
+   ```sh
+   wrangler deploy
+   ```
 
-# 功能
+## 功能
 
 - 代理功能：拦截并通过 Cloudflare Worker 转发到目标网站的请求。
 - 自动 URL 重写：修改 HTML 内容中的相对 URL，使它们通过代理加载。
 - Cookie 记录：在 Cookie 中存储当前访问的目标网站，以便处理后续特殊地直接对根路径请求时不需要再次提供完整的 URL。
 - 重写 fetch 和 XMLHttpRequest：在 HTML 页面中注入 JavaScript，确保通过 fetch 或 XMLHttpRequest 动态加载的内容也通过代理进行。
 
-# 使用方法
+## 使用方法
 
 在任意 url 前面加上 https://你的域名/proxy/ 即可使用 cloudflare 加速。
 
-# 未完成
+## 未完成
 
 使用 service worker 代理全部请求
 
-# acknowledgement: https://github.com/gaboolic/cloudflare-reverse-proxy/tree/main
+## acknowledgement: https://github.com/gaboolic/cloudflare-reverse-proxy/tree/main
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
 addEventListener('fetch', (event) => {
-	event.respondWith(handleRequest(event.request));
+	event.respondWith(handleEvent(event));
 });
 serviceWorker = `
 // public/service-worker.js
@@ -47,82 +49,95 @@ self.addEventListener('fetch', (event) => {
 	}
 });
 `;
-async function handleRequest(request) {
-	const url = new URL(request.url);
-	if (url.pathname === '/service-worker.js') {
-		// 直接返回 Service Worker 脚本内容
-		return new Response(serviceWorker, {
-			headers: { 'Content-Type': 'application/javascript' },
-		});
-	}
-	if (url.pathname === '/' || url.pathname === '/proxy/') {
-		// 将请求代理到 Cloudflare Pages 部署的网站
-		const pagesUrl = 'https://html.paperai.life'; // 将其替换为你的 Pages URL https://pages.paperai.life
-		return fetch(pagesUrl);
-	}
-	let actualUrlStr;
-	if (!url.pathname.startsWith('/proxy/')) {
-		//从cookie中读取之前访问的网站，设置actualUrlStr
-		const cookie = request.headers.get('Cookie');
-		if (cookie) {
-			const cookieObj = Object.fromEntries(
-				cookie.split(';').map((cookie) => {
-					const [key, ...val] = cookie.trim().split('=');
-					return [key.trim(), val.join('=').trim()];
-				})
-			);
-			if (cookieObj.current_site) {
-				actualUrlStr = decodeURIComponent(cookieObj.current_site) + url.pathname + url.search + url.hash;
-				console.log('actualUrlStr in cookieObj:', actualUrlStr);
-				const actualUrl = new URL(actualUrlStr);
-				const redirectUrl = `${url.origin}/proxy/${actualUrl}`;
-				return Response.redirect(redirectUrl, 301);
-			} else {
-				return new Response(
-					`no website in cookie, Please visit a website first,cookie:${JSON.stringify(cookieObj)}, website: ${cookieObj.current_site}`,
-					{
-						status: 400,
-						headers: { 'Content-Type': 'text/plain' },
-					}
-				);
-			}
-		} else {
-			return new Response(`no cookie, Please visit a website first,cookie:${JSON.stringify(cookieObj)}`, {
-				status: 400,
-				headers: { 'Content-Type': 'text/plain' },
+async function handleEvent(event) {
+	try {
+
+
+		const request = event.request;
+		const url = new URL(request.url);
+		if (url.pathname === '/service-worker.js') {
+			// 直接返回 Service Worker 脚本内容
+			return new Response(serviceWorker, {
+				headers: { 'Content-Type': 'application/javascript' },
 			});
 		}
-	} else {
-		// 例如https://14790897.xyz/proxy/https://youtube.com 会访问 https://youtube.com
-		actualUrlStr = url.pathname.replace('/proxy/', '') + url.search + url.hash; //使用的只有pathname
+		if (url.pathname === '/' || url.pathname === '/proxy/') {
+			// Add logic to decide whether to serve an asset or run your original Worker code
+			return await getAssetFromKV(event);
+			// 将请求代理到 Cloudflare Pages 部署的网站
+			// const pagesUrl = 'https://html.paperai.life'; // 将其替换为你的 Pages URL https://pages.paperai.life
+			// return fetch(pagesUrl);
+		}
+		let actualUrlStr;
+		if (!url.pathname.startsWith('/proxy/')) {
+			//从cookie中读取之前访问的网站，设置actualUrlStr
+			const cookie = request.headers.get('Cookie');
+			if (cookie) {
+				const cookieObj = Object.fromEntries(
+					cookie.split(';').map((cookie) => {
+						const [key, ...val] = cookie.trim().split('=');
+						return [key.trim(), val.join('=').trim()];
+					})
+				);
+				if (cookieObj.current_site) {
+					actualUrlStr = decodeURIComponent(cookieObj.current_site) + url.pathname + url.search + url.hash;
+					console.log('actualUrlStr in cookieObj:', actualUrlStr);
+					const actualUrl = new URL(actualUrlStr);
+					const redirectUrl = `${url.origin}/proxy/${actualUrl}`;
+					return Response.redirect(redirectUrl, 301);
+				} else {
+					return new Response(
+						`no website in cookie, Please visit a website first,cookie:${JSON.stringify(cookieObj)}, website: ${cookieObj.current_site}`,
+						{
+							status: 400,
+							headers: { 'Content-Type': 'text/plain' },
+						}
+					);
+				}
+			} else {
+				return new Response(`no cookie, Please visit a website first,cookie:${JSON.stringify(cookieObj)}`, {
+					status: 400,
+					headers: { 'Content-Type': 'text/plain' },
+				});
+			}
+		} else {
+			// 例如https://14790897.xyz/proxy/https://youtube.com 会访问 https://youtube.com
+			actualUrlStr = url.pathname.replace('/proxy/', '') + url.search + url.hash; //使用的只有pathname
+		}
+
+		const actualUrl = new URL(actualUrlStr);
+		console.log('actualUrlStr:', actualUrlStr);
+		const actualOrigin = actualUrl.origin;
+		const modifiedRequest = new Request(actualUrl, {
+			headers: request.headers,
+			method: request.method,
+			body: request.body,
+			redirect: 'follow',
+		});
+
+		let response = await fetch(modifiedRequest);
+		const baseUrl = `${url.origin}/proxy/${actualOrigin}`;
+
+		if (response.headers.get('Content-Type')?.includes('text/html')) {
+			response = await updateRelativeUrls(
+				response,
+				baseUrl // 重新请求
+			);
+		}
+
+		const modifiedResponse = new Response(response.body, response);
+		modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+		// 使用一个cookie来记录当前访问的网站
+		const currentSiteCookie = `current_site=${encodeURIComponent(actualUrl.origin)}; Path=/; HttpOnly; Secure`;
+		modifiedResponse.headers.append('Set-Cookie', currentSiteCookie);
+		return modifiedResponse;
+	} catch (e) {
+		let pathname = new URL(event.request.url).pathname;
+		return new Response(`"${pathname}" not found`, {
+			status: 404,
+			statusText: 'not found',
+		});
 	}
-
-	const actualUrl = new URL(actualUrlStr);
-	console.log('actualUrlStr:', actualUrlStr);
-	const actualOrigin = actualUrl.origin;
-	const modifiedRequest = new Request(actualUrl, {
-		headers: request.headers,
-		method: request.method,
-		body: request.body,
-		redirect: 'follow',
-	});
-
-	let response = await fetch(modifiedRequest);
-	const baseUrl = `${url.origin}/proxy/${actualOrigin}`;
-
-	if (response.headers.get('Content-Type')?.includes('text/html')) {
-		response = await updateRelativeUrls(
-			response,
-			baseUrl // 重新请求
-		);
-	}
-
-	const modifiedResponse = new Response(response.body, response);
-	modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
-	// 使用一个cookie来记录当前访问的网站
-	const currentSiteCookie = `current_site=${encodeURIComponent(actualUrl.origin)}; Path=/; HttpOnly; Secure`;
-	modifiedResponse.headers.append('Set-Cookie', currentSiteCookie);
-	return modifiedResponse;
 }
 
 async function updateRelativeUrls(response, baseUrl) {
