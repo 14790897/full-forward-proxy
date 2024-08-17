2cf19d50a3c86fa7f23830be7812904532a32232
liuweiqing <liuweiqing147@gmail.com>
2024-08-17 17:21:31 +0800

    feat: 静态文件放在kv中

--- src/index.js
+++ src/index.js
@@ -3,64 +3,12 @@ import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 addEventListener('fetch', (event) => {
 	event.respondWith(handleEvent(event));
 });
-serviceWorker = `
-// public/service-worker.js
-self.addEventListener('install', (event) => {
-	console.log('Service Worker installing...');
-	self.skipWaiting();
-});
 
-self.addEventListener('activate', (event) => {
-	console.log('Service Worker activating...');
-	event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-	console.log('Fetch event for:', event.request.url);
-
-	const prefix = 'https://forward.paperai.life/proxy/';
-	const url = new URL(event.request.url);
-
-	// 处理 fetch 请求
-	if (!url.href.startsWith(prefix)) {
-		const modifiedUrl = prefix + url.href;
-		const modifiedRequestInit = {
-			method: event.request.method,
-			headers: event.request.headers,
-			body: event.request.body,
-			mode: event.request.mode,
-			credentials: event.request.credentials,
-			cache: event.request.cache,
-			redirect: event.request.redirect,
-			referrer: event.request.referrer,
-			integrity: event.request.integrity,
-		};
-
-		// 如果有 body，设置 duplex: 'half'
-		if (event.request.body) {
-			modifiedRequestInit.duplex = 'half';
-		}
-
-		const modifiedRequest = new Request(modifiedUrl, modifiedRequestInit);
-		event.respondWith(fetch(modifiedRequest));
-		return;
-	} else {
-		event.respondWith(fetch(event.request));
-	}
-});
-`;
 async function handleEvent(event) {
 	try {
-
-
 		const request = event.request;
 		const url = new URL(request.url);
-		if (url.pathname === '/service-worker.js') {
-			// 直接返回 Service Worker 脚本内容
-			return new Response(serviceWorker, {
-				headers: { 'Content-Type': 'application/javascript' },
-			});
-		}
+
 		if (url.pathname === '/' || url.pathname === '/proxy/') {
 			// Add logic to decide whether to serve an asset or run your original Worker code
 			return await getAssetFromKV(event);
