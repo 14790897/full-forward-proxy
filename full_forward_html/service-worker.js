d9f51819d6b8e58d744a4fbcc04fc6d420175fb6
liuweiqing <liuweiqing147@gmail.com>
2024-08-30 16:25:46 +0800

    refractor: 增加错误捕获

--- full_forward_html/service-worker.js
+++ full_forward_html/service-worker.js
@@ -12,65 +12,69 @@ self.addEventListener('activate', (event) => {
 });
 
 // 定义不需要处理的路径和协议
-const excludedPaths = ['/', '/service-worker.js', '/favicon.ico'];
+const excludedPaths = ['/', '/service-worker.js', '/favicon.ico', '/util.js'];
 const excludedProtocols = ['chrome-extension:', 'about:'];
 
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
 		(async function () {
-			const webRequestUrlObject = new URL(event.request.url); // 用户请求的完整链接
-			if (excludedPaths.includes(webRequestUrlObject.pathname) || excludedProtocols.includes(webRequestUrlObject.protocol)) {
-				return fetch(event.request); // 直接转发，不修改
-			} else {
-				const myWebsiteDomain = new URL(self.location.href).origin; // 我的网站的域名的域名（也就是我的代理网站）
-				const prefix = `${myWebsiteDomain}/`;
+			try {
+				const webRequestUrlObject = new URL(event.request.url); // 用户请求的完整链接
+				if (excludedPaths.includes(webRequestUrlObject.pathname) || excludedProtocols.includes(webRequestUrlObject.protocol)) {
+					return fetch(event.request); // 直接转发，不修改
+				} else {
+					const myWebsiteDomain = new URL(self.location.href).origin; // 我的网站的域名的域名（也就是我的代理网站）
+					const prefix = `${myWebsiteDomain}/`;
 
-				//正常情况下请求的链接应该是我的域名加上需要代理的完整的网址路径，如果请求的是我的代理网站的域名加上不带http域名的路径，说明需要使用上一次请求获得到的需要代理的域名加上去(条件：以我的网站的域名开头,不带 /http 前缀) 但是如果说Href它是一个相对路径的话,好像有问题
-				// 如果它是以别的网站域名为开头的话那么直接加上我的网站域名就行了,见第二个if
-				if (!webRequestUrlObject.pathname.startsWith('/http') && webRequestUrlObject.href.startsWith(myWebsiteDomain)) {
-					// 从 Cache 中获取 lastRequestedDomain
-					const cache = await caches.open('full-proxy-cache');
-					const cachedResponse = await cache.match('lastRequestedDomain');
-					let lastRequestedDomain = cachedResponse ? await cachedResponse.text() : null;
-					console.log('lastRequestedDomain:', lastRequestedDomain);
+					//正常情况下请求的链接应该是我的域名加上需要代理的完整的网址路径，如果请求的是我的代理网站的域名加上不带http域名的路径，说明需要使用上一次请求获得到的需要代理的域名加上去(条件：以我的网站的域名开头,不带 /http 前缀) 但是如果说Href它是一个相对路径的话,好像有问题
+					// 如果它是以别的网站域名为开头的话那么直接加上我的网站域名就行了,见第二个if
+					if (!webRequestUrlObject.pathname.startsWith('/http') && webRequestUrlObject.href.startsWith(myWebsiteDomain)) {
+						// 从 Cache 中获取 lastRequestedDomain
+						const cache = await caches.open('full-proxy-cache');
+						const cachedResponse = await cache.match('lastRequestedDomain');
+						let lastRequestedDomain = cachedResponse ? await cachedResponse.text() : null;
+						console.log('lastRequestedDomain:', lastRequestedDomain);
 
-					if (lastRequestedDomain) {
-						const reconstructedTrueUrl = `${decodeURIComponent(lastRequestedDomain)}${webRequestUrlObject.pathname}${
-							webRequestUrlObject.search
-						}`;
-						const reconstructedUrl = `${prefix}${reconstructedTrueUrl}`;
+						if (lastRequestedDomain) {
+							const reconstructedTrueUrl = `${decodeURIComponent(lastRequestedDomain)}${webRequestUrlObject.pathname}${
+								webRequestUrlObject.search
+							}`;
+							const reconstructedUrl = `${prefix}${reconstructedTrueUrl}`;
 
-						const redirectUrl = new URL(reconstructedUrl);
-						const redirectResponse = Response.redirect(redirectUrl, 308);
-						console.log(
-							'请求的路径不包含完整的 URL,同时它是以我的网站的域名开头,已修改:',
-							redirectUrl.href,
-							'原始请求URL:',
-							webRequestUrlObject.href
-						);
-						return redirectResponse;
-					} else {
-						console.log(`No last requested domain available. webRequestUrlObject: ${webRequestUrlObject.href}`);
-						return new Response('No last requested domain available', { status: 400 });
+							const redirectUrl = new URL(reconstructedUrl);
+							const redirectResponse = Response.redirect(redirectUrl, 308);
+							console.log(
+								'请求的路径不包含完整的 URL,同时它是以我的网站的域名开头,已修改:',
+								redirectUrl.href,
+								'原始请求URL:',
+								webRequestUrlObject.href
+							);
+							return redirectResponse;
+						} else {
+							console.log(`No last requested domain available. webRequestUrlObject: ${webRequestUrlObject.href}`);
+							return new Response('No last requested domain available', { status: 400 });
+						}
 					}
+					// 如果请求的域名不以myWebsiteDomain开头，说明他请求了外部的服务同时那个服务是一个完整的链接，则加上前缀，使得可以代理, 同时我认为这个不是主要的网页所以不将它加入域名的缓存中
+					if (!webRequestUrlObject.href.startsWith(myWebsiteDomain)) {
+						const modifiedUrl = `${prefix}${webRequestUrlObject.href}`;
+						console.log('URL未被添加前缀,已修改:', modifiedUrl, '原始请求URL:', webRequestUrlObject.href);
+						// 这里重定向到新的 URL，暂时不使用
+						const redirectUrl = new URL(modifiedUrl);
+						const redirectResponse = Response.redirect(redirectUrl, 308);
+						return redirectResponse;
+					}
+					// 捕获其他之前未处理的请求
+					console.log('未修改,链接已经符合代理格式：', webRequestUrlObject.href);
+					const response = await fetch(event.request);
+					// 检查响应的 Content-Type 是否为 text/html
+					if (response.headers.get('Content-Type')?.includes('text/html')) {
+						await getUrlOriginPutCache(webRequestUrlObject);
+					}
+					return response;
 				}
-				// 如果请求的域名不以myWebsiteDomain开头，说明他请求了外部的服务同时那个服务是一个完整的链接，则加上前缀，使得可以代理, 同时我认为这个不是主要的网页所以不将它加入域名的缓存中
-				if (!webRequestUrlObject.href.startsWith(myWebsiteDomain)) {
-					const modifiedUrl = `${prefix}${webRequestUrlObject.href}`;
-					console.log('URL未被添加前缀,已修改:', modifiedUrl, '原始请求URL:', webRequestUrlObject.href);
-					// 这里重定向到新的 URL，暂时不使用
-					const redirectUrl = new URL(modifiedUrl);
-					const redirectResponse = Response.redirect(redirectUrl, 308);
-					return redirectResponse;
-				}
-				// 捕获其他之前未处理的请求
-				console.log('未修改,链接已经符合代理格式：', webRequestUrlObject.href);
-				const response = await fetch(event.request);
-				// 检查响应的 Content-Type 是否为 text/html
-				if (response.headers.get('Content-Type')?.includes('text/html')) {
-					await getUrlOriginPutCache(webRequestUrlObject);
-				}
-				return response;
+			} catch (error) {
+				console.error('通用Fetch failed', error, '请求URL:', webRequestUrlObject.href);
 			}
 		})()
 	);
